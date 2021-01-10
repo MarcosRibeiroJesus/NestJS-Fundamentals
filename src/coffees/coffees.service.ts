3f3d0ab8e9b7d044d9719875cbbe3e33f4e32536
Marcos Ribeiro <mrj84@outlook.com>
2021-01-10 15:50:58 -0300

    feat(http_error_message): user-friendly error msg using HttpStatus
    Signed-off-by: Marcos Ribeiro <mrj84@outlook.com>

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { Coffee } from './entities/coffee.entity';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { Coffee } from './entities/coffee.entity'
 
 @Injectable()
 export class CoffeesService {
@@ -13,19 +13,22 @@ export class CoffeesService {
   ];
 
   findAll() {
-    return this.coffees;
+    return this.coffees
   }
 
   findOne(id: string) {
-    return this.coffees.find(item => item.id === +id);
+    const coffee = this.coffees.find(item => item.id === +id)
+    if(!coffee) throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND)
+
+    return coffee
   }
 
   create(createCoffeeDto: any) {
-    this.coffees.push(createCoffeeDto);
+    this.coffees.push(createCoffeeDto)
   }
 
   update(id: string, updateCoffeeDto: any) {
-    const existingCoffee = this.findOne(id);
+    const existingCoffee = this.findOne(id)
     if (existingCoffee) {
       // update the existing entity
     }
